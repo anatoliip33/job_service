0fb8ce94b6d9f267e9082c3be640854950d74c0a
anatolii <anatoliipolishchuk@gmail.com>
2025-03-13 19:30:58 +0200

    fix sorting issue and infinite loop

--- lib/job_service/helper.ex
+++ lib/job_service/helper.ex
@@ -6,18 +6,36 @@ defmodule JobService.Helper do
   @doc """
   Sorts tasks based on requires.
   """
-  def sort_tasks(tasks) do
-    tasks
-    |> Enum.sort_by(fn task ->
-      case task["requires"] do
-        requires when is_list(requires) and requires !== [] ->
-          requires
-          |> Enum.sort(:desc)
+  def sort_tasks(tasks, sorted_tasks \\ [])
 
-        _ ->
-          task
-      end
-    end)
-    |> Enum.map(&(&1 |> Map.drop(["requires"])))
+  def sort_tasks([], [nil | _]), do: [%{"command" => "Bad arguments (infinite loop)"}]
+  def sort_tasks([], sorted_tasks), do: sorted_tasks |> Enum.reverse() |> Enum.map(& &1 |> Map.drop(["requires"]))
+
+  def sort_tasks(tasks, sorted_tasks) do
+    {start_tasks, rest_tasks} =
+      tasks
+      |> Enum.split_with(& is_nil(&1["requires"]) || length(&1["requires"]) == 0)
+
+    required_task_names =
+      tasks
+      |> Enum.map(& &1["requires"])
+      |> List.flatten()
+      |> Enum.frequencies()
+
+    start_task =
+      start_tasks
+      |> Enum.sort_by(fn task -> required_task_names[task["name"]] end, :desc)
+      |> List.first()
+
+    rest_tasks =
+      rest_tasks
+      |> Enum.map(fn task ->
+        %{
+          task | "requires" => task["requires"] |> List.delete(start_task["name"])}
+      end)
+
+    rest_tasks = start_task && rest_tasks || []
+
+    sort_tasks(rest_tasks, [start_task | sorted_tasks])
   end
 end
--- test/job_service_test.exs
+++ test/job_service_test.exs
@@ -10,15 +10,15 @@ defmodule JobServiceTest do
   @sample_payload %{
     "tasks" =>
       Enum.shuffle([
-        %{"name" => "task-1", "command" => "touch /tmp/file1"},
+        %{"name" => "task-5", "command" => "touch /tmp/file1"},
         %{"name" => "task-2", "command" => "cat /tmp/file1", "requires" => ["task-3"]},
-        %{"name" => "task-3", "command" => "echo 'Hello World!' > /tmp/file1", "requires" => ["task-1"]},
+        %{"name" => "task-3", "command" => "echo 'Hello World!' > /tmp/file1", "requires" => ["task-5"]},
         %{"name" => "task-4", "command" => "rm /tmp/file1", "requires" => ["task-2", "task-3"]}
       ])
   }
   |> Jason.encode!()
 
-  @etalon_example ["task-1", "task-3", "task-2", "task-4"]
+  @etalon_example ["task-5", "task-3", "task-2", "task-4"]
 
   test "POST /jobs returns sorted tasks in JSON" do
     conn =
@@ -38,5 +38,63 @@ defmodule JobServiceTest do
 
     assert conn.status == 200
     assert String.starts_with?(conn.resp_body, "#!/usr/bin/env bash")
+  end
+
+  test "POST /jobs returns sorted tasks in JSON (revisited)" do
+    conn =
+      conn(
+        :post,
+        "/jobs",
+        Jason.encode!(%{
+          "tasks" => [
+            %{"name" => "task-5", "command" => "touch /tmp/file1"},
+            %{"name" => "task-2", "command" => "cat /tmp/file1", "requires" => ["task-3"]},
+            %{
+              "name" => "task-3",
+              "command" => "echo 'Hello World!' > /tmp/file1",
+              "requires" => ["task-5"]
+            },
+            %{
+              "name" => "task-4",
+              "command" => "rm /tmp/file1",
+              "requires" => ["task-2", "task-3"]
+            }
+          ]
+        })
+      )
+      |> put_req_header("content-type", "application/json")
+      |> Router.call(@opts)
+
+    assert conn.status == 200
+    assert Jason.decode!(conn.resp_body)["tasks"] |> Enum.map(& &1["name"]) == ["task-5", "task-3", "task-2", "task-4"]
+  end
+
+  test "POST /jobs returns sorted tasks in JSON (infinite loop)" do
+    conn =
+      conn(
+        :post,
+        "/jobs",
+        Jason.encode!(%{
+          "tasks" => [
+            %{"name" => "task-5", "command" => "touch /tmp/file1"},
+            %{"name" => "task-2", "command" => "cat /tmp/file1", "requires" => ["task-3"]},
+            %{
+              "name" => "task-3",
+              "command" => "echo 'Hello World!' > /tmp/file1",
+              "requires" => ["task-5", "task-2"]
+            },
+            %{
+              "name" => "task-4",
+              "command" => "rm /tmp/file1",
+              "requires" => ["task-2", "task-3"]
+            }
+          ]
+        })
+      )
+      |> put_req_header("content-type", "application/json")
+      |> Router.call(@opts)
+
+    assert conn.status == 200
+    assert Jason.decode!(conn.resp_body)["tasks"] |> List.first() |> Map.get("command") == "Bad arguments (infinite loop)"
   end
 end
